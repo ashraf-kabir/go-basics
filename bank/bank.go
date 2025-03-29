d3d2766fe7a9e09a36b9885a2546e562a07a5433
ashraf-kabir <ashrafkabir95@gmail.com>
2025-03-29 18:14:55 +0600

    bank app prints

--- bank/bank.go
+++ bank/bank.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the GO BANK!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Println("Your choice: ")
+	fmt.Scan(&choice)
+
+	fmt.Println("Your choice: ", choice)
+}
