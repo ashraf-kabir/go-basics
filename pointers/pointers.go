bedc4967407365217341fc2b79cecc42ec95665c
ashraf-kabir <ashrafkabir95@gmail.com>
2025-04-14 09:47:16 +0600

    pointers using on mutation

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -11,11 +11,14 @@ func main() {
 	fmt.Println("agePointer: ", agePointer) // prints age pointer
 	fmt.Println("age: ", *agePointer) // prints age, de-reference
 
-	adultYears := getAdultYears(&age)
-	fmt.Println(adultYears)
+	// adultYears := getAdultYears(&age)
+	// fmt.Println(adultYears)
+	getAdultYears(&age)
+	fmt.Println("age: ", age)
 }
 
 // avoiding a copy of integer by using pointer
-func getAdultYears(age *int) int {
-	return *age - 18
+func getAdultYears(age *int) {
+	// return *age - 18
+	*age = *age - 18
 }
