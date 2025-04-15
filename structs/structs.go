2d74fc5a7f952c31f6794114eaba5f083064679d
ashraf-kabir <ashrafkabir95@gmail.com>
2025-04-15 17:05:58 +0600

    struct declaration

--- structs/structs.go
+++ structs/structs.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type user struct {
+	firstName string
+	lastName string
+	birthdate string
+	createdAt time.Time
+}
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
+
+	appUser = user{
+		firstName,
+		lastName,
+		birthdate,
+		time.Now(),
+	}
 
 	// ... do something awesome with that gathered data!
 
