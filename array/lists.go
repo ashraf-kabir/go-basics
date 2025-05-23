ca93008edcd1c6eccbfd9499b39bab2a3c4a9d4f
ashraf-kabir <ashrafkabir95@gmail.com>
2025-05-24 01:57:57 +0600

    dynamic list using slices

--- array/lists.go
+++ array/lists.go
@@ -2,36 +2,48 @@ package main
 
 import "fmt"
 
+//func main() {
+//	var productNames [3]string
+//	productNames[0] = "A"
+//	productNames[1] = "B"
+//	productNames[2] = "C"
+//
+//	prices := [3]float64{1.99, 2.99, 3.99}
+//
+//	prices[1] = 4.99 // changing the value of prices[1] will also change the value of featuredPrices2 and featuredPrices3
+//
+//	fmt.Println(prices)
+//	fmt.Println(prices[0])
+//	fmt.Println(len(prices))
+//	fmt.Println(productNames)
+//	fmt.Println(len(productNames))
+//
+//	// Slices
+//	featuredPrices1 := prices[0:2] // start from index 0 and ends before index 2
+//	featuredPrices2 := prices[:2]  // start from beginning and ends before index 2
+//	featuredPrices3 := prices[1:]  // start index 1 and ends at the end
+//
+//	fmt.Println(featuredPrices1)
+//	fmt.Println(featuredPrices2)
+//	fmt.Println(featuredPrices3)
+//
+//	fmt.Println(len(prices)) // length of the array
+//	fmt.Println(cap(prices)) // capacity of the array
+//
+//	featuredPrices1 = featuredPrices1[:3]
+//
+//	fmt.Println(len(featuredPrices1)) // length of the slice
+//	fmt.Println(cap(featuredPrices1)) // capacity of the slice
+//}
+
 func main() {
-	var productNames [3]string
-	productNames[0] = "A"
-	productNames[1] = "B"
-	productNames[2] = "C"
+	prices := []float64{1.99, 2.56}
+	//fmt.Println(prices[0:1])
 
-	prices := [3]float64{1.99, 2.99, 3.99}
+	prices[1] = 3.99
+	//prices[3] = 2.56
+	//fmt.Println(prices)
 
-	prices[1] = 4.99 // changing the value of prices[1] will also change the value of featuredPrices2 and featuredPrices3
-
-	fmt.Println(prices)
-	fmt.Println(prices[0])
-	fmt.Println(len(prices))
-	fmt.Println(productNames)
-	fmt.Println(len(productNames))
-
-	// Slices
-	featuredPrices1 := prices[0:2] // start from index 0 and ends before index 2
-	featuredPrices2 := prices[:2]  // start from beginning and ends before index 2
-	featuredPrices3 := prices[1:]  // start index 1 and ends at the end
-
-	fmt.Println(featuredPrices1)
-	fmt.Println(featuredPrices2)
-	fmt.Println(featuredPrices3)
-
-	fmt.Println(len(prices)) // length of the array
-	fmt.Println(cap(prices)) // capacity of the array
-
-	featuredPrices1 = featuredPrices1[:3]
-
-	fmt.Println(len(featuredPrices1)) // length of the slice
-	fmt.Println(cap(featuredPrices1)) // capacity of the slice
+	updatedPrices := append(prices, 4.99) // add a new element to the slice
+	fmt.Println(updatedPrices, prices)
 }
