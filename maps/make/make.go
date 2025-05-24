0c4a08811dd95c5b811fd716e742f3ef63701f09
ashraf-kabir <ashrafkabir95@gmail.com>
2025-05-24 23:13:32 +0600

    make, type alias

---
+++ maps/make/make.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
+func main() {
+	//userNames := []string{}
+	userNames := make([]string, 2, 5) // type, length, capacity
+
+	userNames[0] = "Max"
+
+	userNames = append(userNames, "John")
+	userNames = append(userNames, "Jane")
+
+	fmt.Println(userNames)
+
+	//courseRatings := map[string]float64{}
+	//courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
+
+	courseRatings["go"] = 4.5
+	courseRatings["java"] = 4.8
+	courseRatings["php"] = 5.00
+
+	//fmt.Println(courseRatings)
+	courseRatings.output()
+}
