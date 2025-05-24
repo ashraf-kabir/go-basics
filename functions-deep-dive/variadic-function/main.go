ba9625cb7df996004d9ad271b294606c3f7527e5
ashraf-kabir <ashrafkabir95@gmail.com>
2025-05-25 01:55:04 +0600

    splitting slices into param values

--- functions-deep-dive/variadic-function/main.go
+++ functions-deep-dive/variadic-function/main.go
@@ -3,16 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	// sum := sumup(numbers)
 
 	sum := sumup(1, 2, 3)
+	anotherSum := sumup(1, numbers...)
 
 	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
 // variadic function that takes any number of args
-func sumup(numbers ...int) int {
+// first arg added as example & not used
+func sumup(startingValue int, numbers ...int) int {
 	sum := 0
 
 	for _, v := range numbers {
