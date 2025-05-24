a366ef2fc7bfdd7280de34bfc96776488adeed22
ashraf-kabir <ashrafkabir95@gmail.com>
2025-05-24 23:55:42 +0600

    returning function as values

--- functions-deep-dive/main.go
+++ functions-deep-dive/main.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
+type transformFn func(int) int // custom type
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
+	moreNumbers := []int{6, 7, 8, 9, 10}
+
 	doubledNumbers := transformNumbers(&numbers, double)
-	fmt.Println(doubledNumbers)
 	tripledNumbers := transformNumbers(&numbers, triple)
+
+	fmt.Println(doubledNumbers)
 	fmt.Println(tripledNumbers)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
 }
 
 // function type on transform argument
@@ -19,6 +31,15 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 		dNumbers = append(dNumbers, transform(v))
 	}
 	return dNumbers
+}
+
+// takes pointer to the slice
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
