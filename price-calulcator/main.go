adcbe122903c984daa36549bc324b658b8b8284d
ashraf-kabir <ashrafkabir95@gmail.com>
2025-05-30 14:33:45 +0600

    parallel process added

--- price-calulcator/main.go
+++ price-calulcator/main.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"fmt"
-	"price-calulcator/cmdmanager"
+	"price-calulcator/filemanager"
 	"price-calulcator/prices"
 )
 
 func main() {
 	taxRates := []float64{0.4, 0.5, 0.6}
+	doneChans := make([]chan bool, len(taxRates))
 
-	for _, taxRate := range taxRates {
-		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		cmdm := cmdmanager.New()
-		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
-		err := priceJob.Process()
+	for i, taxRate := range taxRates {
+		doneChans[i] = make(chan bool)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		go priceJob.Process(doneChans[i])
 
-		if err != nil {
-			fmt.Println("Could not process price job")
-			fmt.Println(err)
-		}
+		// if err != nil {
+		// 	fmt.Println("Could not process price job")
+		// 	fmt.Println(err)
+		// }
+	}
+
+	for _, doneChan := range doneChans {
+		<-doneChan
 	}
 }
